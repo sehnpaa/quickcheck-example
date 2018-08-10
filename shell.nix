0328f6c858385f0a28ac7d8cc2a50449ac3eafb3
Peter Andersson <sehnpaa@gmail.com>
2018-08-10 15:01:54 +0200

    Use latest compiler

--- shell.nix
+++ shell.nix
@@ -1,4 +1,4 @@
-{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc802" }:
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc843" }:
 let
   inherit (nixpkgs) pkgs;
   ghc = pkgs.haskell.packages.${compiler}.ghcWithPackages (ps: with ps; [
